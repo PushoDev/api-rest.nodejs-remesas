dfe1a9fe3accd456999a5c14ef12894a377fbfa3
PushoDev <bethocubans1990@gmail.com>
2024-12-02 16:26:56 -0500

    Deploy in Vercel - Config vercel.json

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+// Router
+app.get("/", (req, res) => {
+  res.send("Hola Mundo");
+});
+
 app.get("/api/clients", (req, res) => {
   res.send("List Clients");
 });
